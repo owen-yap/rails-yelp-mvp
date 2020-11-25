ef29939504c7786de004688f5335308c98ca7512
Owen Yap <owen.y.yuhan@gmail.com>
2020-11-25 11:40:22 +0800

    Prepare rails app with external specs

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,6 @@
 class Restaurant < ApplicationRecord
   has_many :reviews
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"],
+    message: "%{value} is not a valid category" }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+
+  validates :restaurant_id, :content, :rating, presence: true
+  validates :rating, numericality: { only_integer: true, less_than_or_equal_to: 5,
+                                     greater_than_or_equal_to: 0 }
 end
